b4bd9491421e66bca0e898741b9a0e9586262010
Sina Samangooei <sinjax@gmail.com>
2013-03-15 13:27:57 +0000

    trying something

--- text/nlp/src/main/java/org/openimaj/text/nlp/textpipe/annotators/AbstractNEAnnotator.java
+++
@@ -1,7 +0,0 @@
-package org.openimaj.text.nlp.textpipe.annotators;
-
-import org.openimaj.text.nlp.textpipe.annotations.RawTextAnnotation;
-
-public abstract class AbstractNEAnnotator extends AbstractTextPipeAnnotator<RawTextAnnotation>{
-		
-}
--- text/nlp/src/main/java/org/openimaj/text/nlp/textpipe/annotators/AbstractRawTextAnnotator.java
+++
@@ -1,9 +0,0 @@
-package org.openimaj.text.nlp.textpipe.annotators;
-
-import org.openimaj.text.nlp.textpipe.annotations.RawTextAnnotation;
-
-public abstract class AbstractRawTextAnnotator{
-	
-	abstract RawTextAnnotation getRawTextAnnotation(String rawText);
-
-}
