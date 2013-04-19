04525bc5f9c9be4bf799b95d9aa65366ff0f2815
Sina Samangooei <sinjax@gmail.com>
2013-04-19 10:57:48 +0100

    new binfile

---
+++ bin/streamdampexp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+pushd . >> /dev/null
+cd $DIR/..
+# pwd
+MLHOME=machine-learning/machine-learning
+MLJAR=$MLHOME/target/machine-learning-1.0.0-SNAPSHOT.jar
+
+if [ ! -f $MLJAR ]
+then
+	mvn install -DskipTests
+fi
+
+if [ ! -f $MLHOME/cp.txt ]
+then
+	mvn -pl $MLHOME dependency:build-classpath -Dmdep.outputFile=cp.txt
+fi
+
+EXPCP=`cat $MLHOME/cp.txt`:$MLJAR
+EXPCLASS=org.openimaj.ml.linear.experiments.StreamAustrianDampeningExperiments
+java -Xmx2G -cp $EXPCP $EXPCLASS
+popd >> /dev/null
