0af3a25277c3cf2b2bad68cf3ef91d4490d52fa7
Sina Samangooei <sinjax@gmail.com>
2013-09-28 08:58:38 +0100

    missing files

---
+++ tools/TwitterPreprocessingWebService/src/main/java/org/openimaj/webservice/twitter/PreProcessFileUploadTask.java
@@ -0,0 +1,123 @@
+package org.openimaj.webservice.twitter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileItemHeaders;
+import org.apache.commons.fileupload.FileItemHeadersSupport;
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileUpload;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.FileUploadBase.FileUploadIOException;
+import org.apache.commons.fileupload.FileUploadBase.IOFileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.util.Streams;
+import org.apache.log4j.Logger;
+import org.openimaj.logger.LoggerUtils;
+import org.openimaj.tools.twitter.modes.preprocessing.TwitterPreprocessingMode;
+import org.openimaj.twitter.GeneralJSON;
+import org.openimaj.twitter.USMFStatus;
+import org.openimaj.twitter.collection.StreamTwitterStatusList;
+import org.openimaj.twitter.collection.TwitterStatusListUtils;
+import org.restlet.Request;
+import org.restlet.ext.fileupload.RepresentationContext;
+import org.restlet.ext.fileupload.RestletFileUpload;
+
+class PreProcessFileUploadTask extends FileUpload implements Runnable {
+
+	private Request req;
+	private PreProcessAppOptions options;
+//	private List<FileItem> items;
+//	private FileItem fi;
+	private static Logger logger = Logger.getLogger(PreProcessFileUploadTask.class);
+
+	public PreProcessFileUploadTask(Request request, PreProcessAppOptions options) throws IOException {
+		this.req = request;
+		this.options = options;
+	}
+
+	@Override
+	public void run() {
+		try{
+			logger.debug("Parsing the request");
+			parseRequest(new RepresentationContext(req.getEntity()));
+			
+		} catch(Exception e){
+			e.printStackTrace();
+			return;
+		}
+		options.close();
+	}
+	
+	
+	
+	public List<FileItem> parseRequest(RepresentationContext ctx) throws FileUploadException {
+		try {
+            FileItemIterator iter = getItemIterator(ctx);
+            
+            while (iter.hasNext()) {
+                FileItemStream item = iter.next();
+                if(!item.getFieldName().equals("data")){
+                	throw new FileUploadException(String.format("File item found not called 'data', called: '%s', failing",item.getName()));
+//                	continue;
+                }
+                try {
+                    consumeStream(item.openStream());
+                } catch (FileUploadIOException e) {
+                    throw (FileUploadException) e.getCause();
+                } catch (IOException e) {
+                    throw new IOFileUploadException("Processing of " + MULTIPART_FORM_DATA + " request failed. " + e.getMessage(), e);
+                } catch (Exception e) {
+                    throw new FileUploadException(e.getMessage(),e);
+                }
+            }
+            return null;
+        } catch (FileUploadIOException e) {
+            throw (FileUploadException) e.getCause();
+        } catch (IOException e) {
+            throw new FileUploadException(e.getMessage(), e);
+        }
+		
+	}
+
+	private void consumeStream(InputStream fi) throws Exception {
+		List<USMFStatus> list = StreamTwitterStatusList.readUSMF(fi, options.getOutputClass().type(), "UTF-8");
+		long seen = 0;
+		for (USMFStatus usmfStatus : list) {
+			LoggerUtils.debug(logger, String.format("Processing item: %d",seen++), seen%1000==0);
+			processStatus(usmfStatus, options);
+		}
+	}
+
+	private void processStatus(USMFStatus usmfStatus,PreProcessAppOptions options) throws Exception {
+		if(usmfStatus.isInvalid() || usmfStatus.text.isEmpty()){
+			if(options.veryLoud()){
+				logger.debug("\nTWEET INVALID, skipping.");
+			}
+			return;
+		}
+		if(options.preProcessesSkip(usmfStatus)) return;
+		for (TwitterPreprocessingMode<?> mode : options.modeOptionsOp) {
+			try {
+				TwitterPreprocessingMode.results(usmfStatus, mode);
+			} catch (Exception e) {
+				logger.error(String.format("Problem producing %s for %s",
+						usmfStatus.id, mode.toString()), e);
+			}
+		}
+		
+		if(options.postProcessesSkip(usmfStatus)) return;
+		PrintWriter outputWriter = options.getOutputWriter();
+		options.ouputMode().output(options.convertToOutputFormat(usmfStatus), outputWriter);
+		outputWriter.println();
+		outputWriter.flush();
+	}
+
+}
---
+++ tools/TwitterPreprocessingWebService/src/test/java/org/openimaj/webservice/twitter/TestSimultaniousMongoDB.java
@@ -0,0 +1,81 @@
+package org.openimaj.webservice.twitter;
+
+import java.net.UnknownHostException;
+import java.util.Random;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.openimaj.util.parallel.GlobalExecutorPool;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
+public class TestSimultaniousMongoDB {
+	static class WriteWait implements Runnable{
+		
+		private DB mdb;
+		public WriteWait() throws UnknownHostException {
+			MongoClient mdb = new MongoClient("localhost");
+			this.mdb = mdb.getDB("testdb");
+			this.mdb.dropDatabase();
+			
+		}
+		@Override
+		public void run() {
+			DBCollection col = mdb.getCollection("testcol");
+			col.ensureIndex("count");
+			int count = 0;
+			while(true){
+				BasicDBObject basicDBObject = new BasicDBObject("cheese",new Random().nextDouble());
+				basicDBObject.put("count", count++);
+				col.insert(basicDBObject);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	static class ReadFast implements Runnable{
+		
+		private DB mdb;
+		public ReadFast() throws UnknownHostException {
+			MongoClient mdb = new MongoClient("localhost");
+			this.mdb = mdb.getDB("testdb");
+			
+		}
+		@Override
+		public void run() {
+			DBCollection col = mdb.getCollection("testcol");
+			int seen = -1;
+			while(true){
+				DBCursor all = col.find(greaterThan(seen)).sort(new BasicDBObject("count", "-1"));
+				while(all.hasNext()){
+					DBObject obj = all.next();
+					System.out.println(obj.get("cheese"));
+					seen = (int) obj.get("count");
+				}
+			}
+		}
+		private DBObject greaterThan(int seen) {
+			return new BasicDBObject("count", new BasicDBObject("$gt", seen));
+		}
+	}
+	
+	public static void main(String[] args) throws UnknownHostException, InterruptedException {
+		ThreadPoolExecutor pool = GlobalExecutorPool.getPool();
+		pool.execute(new WriteWait());
+		pool.execute(new ReadFast());
+		pool.shutdown();
+		pool.awaitTermination(2, TimeUnit.MINUTES);
+	}
+}
